265c46b0bea4aefcd46a6b64a30191f256655d3d
Mathéba <ndxendernight@gmail.com>
2026-02-06 16:57:29 +0100

    feat: cleaned up

--- modules/hosts/framework/configuration.nix
+++ modules/hosts/framework/configuration.nix
@@ -1,10 +1,46 @@
 { inputs, ... }:
 {
-  flake.modules.nixos.framework = { pkgs, ... }: {
+  flake.modules.nixos.framework = { lib, config, pkgs, modulesPath, ... }: {
     imports = [
       inputs.nixos-hardware.nixosModules.framework-13th-gen-intel
       inputs.home-manager.nixosModules.home-manager
+      (modulesPath + "/installer/scan/not-detected.nix")
     ];
+
+    boot.initrd.availableKernelModules = [
+      "xhci_pci"
+      "thunderbolt"
+      "nvme"
+      "usb_storage"
+      "usbhid"
+      "sd_mod"
+    ];
+    boot.initrd.kernelModules = [ ];
+    boot.kernelModules = [ "kvm-intel" ];
+    boot.extraModulePackages = [ ];
+
+    fileSystems."/" = {
+      device = "/dev/disk/by-uuid/87f36fab-3d52-434f-af4c-ed9f30569981";
+      fsType = "ext4";
+    };
+
+    fileSystems."/boot" = {
+      device = "/dev/disk/by-uuid/E6FF-1223";
+      fsType = "vfat";
+      options = [
+        "fmask=0077"
+        "dmask=0077"
+      ];
+    };
+
+    swapDevices = [
+      { device = "/dev/disk/by-uuid/f1b2f53b-bb38-4073-b750-6fa555de0c64"; }
+    ];
+
+    networking.useDHCP = lib.mkDefault true;
+
+    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+    hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
     networking = {
       hostName = "framework";
